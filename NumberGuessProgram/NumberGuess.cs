35252955e7cbc32cad4f394465b1836eacf75a38
Daniel Pereira <77496752+pere0234@users.noreply.github.com>
2021-05-01 07:27:29 -0400

    Update NumberGuess.cs

--- NumberGuessProgram/NumberGuess.cs
+++ NumberGuessProgram/NumberGuess.cs
@@ -1,5 +1,9 @@
 ﻿using System;
-
+/*
+*A simple random number guessing game
+*A random number is generated and the user must guess correctly within 4 turns
+*By: Daniel Pereira
+*/
 namespace NumberGuessingGame
 {
     class NumberGuess
@@ -10,19 +14,25 @@ namespace NumberGuessingGame
             int number, numGuessed;
             byte turnCounter, exitCounter;
             string line;
-            //Generates a random numberber from 1-20
+            //Generates a random number
             Random ran = new Random();
             do{
+            //Resolves the random number to a 32-bit int
             number = Convert.ToInt32(ran.Next(100));
+            //While the number is between 1 and 20, run the loop
             }while(!( ((number >= 1) & (number <= 20))));
             turnCounter = 1; exitCounter = 0;
             do{
+            //Prompts user to guess the number
             Console.Write("Guess the number: "); line = Console.ReadLine();
+            //Reads the user input
             numGuessed = int.Parse(line);
+            //If number guessed equals the generated number, guess is correct
             if( (numGuessed == number) ){
             Console.WriteLine("You guessed correctly!");
             exitCounter = 1;
             }
+            //Else if the guess is either too high or too low, prompt user to guess again
             else{
                 if( (numGuessed > number) ){
                 Console.WriteLine("Guess lower");
@@ -32,6 +42,7 @@ namespace NumberGuessingGame
             }
         }
         turnCounter++;
+        //If user doesn't guess within 4 turns, reveal the number
         }while( ((turnCounter <= MAX_TURNS) & (exitCounter == 0)));
             if( (exitCounter == 0) ){
             Console.Write("The number was: " + number);
